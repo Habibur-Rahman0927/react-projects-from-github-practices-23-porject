24c18fb74d982adcc6ad05e9b3227fb7e5ae2440
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-07-10 14:04:09 -0700

    React basic practice base  project

---
+++ 04-accordion/setup/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <div className="container">
+      <h3>questions and answer about login</h3>
+      <section className="info">
+        {
+          questions.map(qus => <SingleQuestion key={qus.id} {...qus}></SingleQuestion>)
+        }
+      </section>
+    </div>
+  </main>;
+}
+
+export default App;
---
+++ 10-grocery-bud/setup/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, seTeditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg:'', type:''});
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('Hello');
+  }
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert></Alert>}
+        <h3>grocery budy</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" placeholder="e.g. eggs"
+          value={name} onChange={e => setName(e.target.value)}/>
+          <button className="submit-btn" type="submit">
+            {isEditing ? 'edit': 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List></List>
+        <button className="clear-btn">Clear Item</button>
+      </div>
+    </section>
+  )
+}
+
+export default App
