190622969dc466e525d78744f1d020e2c730fbeb
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-07-24 10:08:59 -0700

    add 15 project

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,33 +2,94 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = ()=>{
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'));
+  }
+  else{
+    return [];
+  }
+}
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, seTeditID] = useState(null);
-  const [alert, setAlert] = useState({show: false, msg:'', type:''});
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    console.log('Hello');
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+      // setAlert({show:true, msg:'please enter value', type:'danger'})
+      showAlert(true, 'danger', 'please enter value')
+    }
+    else if (name && isEditing) {
+      // deal with edit
+      setList(list.map((item)=>{
+        if(item.id ===editID){
+          return {...item, title: name}
+        }
+        return item;
+      }));
+      setName('');
+      seTeditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+    }
+    else {
+      // show alert
+      showAlert(true, 'success', 'item add to the list')
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
+      
+    }
+  }
+  const clearList = () =>{
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+  const removeItem = (id) =>{
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) =>{
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    seTeditID(id);
+    setName(specificItem.title);
+  };
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list])
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert></Alert>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}></Alert>}
         <h3>grocery budy</h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e.g. eggs"
-          value={name} onChange={e => setName(e.target.value)}/>
+            value={name} onChange={e => setName(e.target.value)} />
           <button className="submit-btn" type="submit">
-            {isEditing ? 'edit': 'submit'}
+            {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List></List>
-        <button className="clear-btn">Clear Item</button>
-      </div>
+      {
+        list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItem} editItem={editItem}></List>
+            <button className="clear-btn" onClick={clearList}>Clear Item</button>
+          </div>
+        )
+      }
+
     </section>
   )
 }
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem voluptatibus tenetur delectus at minima, tempora distinctio enim est inventore quam excepturi, praesentium ipsam! Illum repudiandae quidem reprehenderit omnis nisi nihil aut ipsam modi hic, ab eos dolores architecto fugit sapiente laborum qui dolor quibusdam neque rem non veniam! Impedit, corporis.</p>
+    </section>
   )
 }
 
